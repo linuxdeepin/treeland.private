48ae4b4823b3bf52afa257303198cab2531e90c8
Zhang Dingyuan <zhangdingyuan@deepin.org>
2024-11-07 11:12:49 +0800

    feat: add layershell animation
    add animation
    
    Log:

--- src/treeland/surfacewrapper.cpp
+++ src/treeland/surfacewrapper.cpp
@@ -633,6 +633,12 @@ void SurfaceWrapper::createNewOrClose(uint direction)
                                  ->scope);
         if (scope == "dde-shell/launchpad") {
             m_windowAnimation = m_engine->createLaunchpadAnimation(this, direction, m_container);
+        } else if (scope == "dde-shell/dock") {
+            auto *surface = qobject_cast<WLayerSurface *>(m_shellSurface);
+            m_windowAnimation = m_engine->createLayerShellAnimation(this, container(), direction);
+            m_windowAnimation->setProperty("position",
+                                           QVariant::fromValue(surface->getExclusiveZoneEdge()));
+            m_windowAnimation->setProperty("enableBlur", true);
         }
     }; break;
     case Type::XdgPopup: // NOTE: check z order for XdgToplevel parent/child after support popup animation
